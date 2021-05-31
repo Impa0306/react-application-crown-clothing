d4bc231ae95a88f0e0513b291e12ea0c5f02d7e7
impa s <impa1994@gmail.com>
2021-05-31 20:24:03 +0530

    updated our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch> 
+      <Route exact path='/' component={HomePage} />
+    </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+//Switch component is an alternative for exact property in the Route component, but requires path to be in order
+//<Switch></Switch  <--- <Route exact path='' component=''/>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,10 +47,17 @@ class Directory extends React.Component {
 
     render() { 
         return ( 
+            // <div className="directory-menu">
+            // {
+            //     this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+            //         <MenuItem key = {id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
+            //     ))
+            // }
+            // </div>
             <div className="directory-menu">
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key = {id} title={title} imageUrl={imageUrl} size={size}/>
+                this.state.sections.map(({id, ...othersectionProps}) => (
+                    <MenuItem key = {id} {...othersectionProps}/>
                 ))
             }
             </div>
